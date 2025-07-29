87d86ef7b5f8469eab59e5fc70ee0eb0ad0ad428
Adam Twardoch <adam+github@twardoch.com>
2025-07-29 13:00:36 +0200

    v1.0.31

--- src/claif_cod/_version.py
+++ src/claif_cod/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.0.29.dev0+g14c077b.d20250729'
-__version_tuple__ = version_tuple = (1, 0, 29, 'dev0', 'g14c077b.d20250729')
+__version__ = version = '1.0.31.dev0+gee87cbf.d20250729'
+__version_tuple__ = version_tuple = (1, 0, 31, 'dev0', 'gee87cbf.d20250729')
